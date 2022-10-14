6909f0e4a4ae9b959fd11609b9051b2ab0982089
Gizer <gabriellartey16@gmail.com>
2022-10-14 23:40:24 +0000

    data type sizes updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 /**
  * main - Entry
+ * Initialize char, int, long int, long long int, float
  * Return: Always 0
  */
 int main(void)
 {
-	printf("Size of a char: %i byte(s)", sizeof(char));
-	printf("Size of a int: %i byte(s)", sizeof(int));
-	printf("Size of a long int: %i byte(s)", sizeof(long int));
-	printf("Size of a long long int: %i byte(s)", sizeof(long long int));
-	printf("Size of a float: %i byte(s)", sizeof(float));
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %i byte(s)\n", sizeof(c));
+	printf("Size of a int: %i byte(s)\n", sizeof(i));
+	printf("Size of a long int: %i byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
+	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
