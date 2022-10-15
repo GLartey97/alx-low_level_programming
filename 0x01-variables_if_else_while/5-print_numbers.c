4239e00318d6cb410095c144b1c4af411315043b
Gizer <gabriellartey16@gmail.com>
2022-10-15 18:39:03 +0000

    base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - all base 10 digits
+ * initialize i
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	printf('\n');
+	return (0);
+}
