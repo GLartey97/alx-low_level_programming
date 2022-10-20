172616f55b140b5193db9cd48a97687fa50078cf
Gizer <gabriellartey16@gmail.com>
2022-10-20 01:41:00 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: number
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	if (ls < 0)
+		ld *= -1;
+
+	_putchar(ls + '0');
+
+	return (0);
+}
