2b4a6c6bb4705d067adddeb8ac38144258f96647
Gizer <gabriellartey16@gmail.com>
2022-11-03 23:06:15 +0000

    recursing a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts a string
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
