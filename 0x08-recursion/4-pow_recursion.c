d435d92c9de7f0e17747093da3ea94b7e4a3feb6
Gizer <gabriellartey16@gmail.com>
2022-11-04 01:08:59 +0000

    exponents

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints a value raised to the power
+ * @x: int value
+ * @y: int exponent
+ * Return: if y less than 0 -1 else pow
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (x < 0)
+		return (-1);
+	if (x == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
