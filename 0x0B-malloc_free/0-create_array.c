4fd1090b468fd46ab400d2c0760c494400c2e151
Gizer <gabriellartey16@gmail.com>
2022-11-10 02:29:07 +0000

    first commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_array - creates array of chars
+ * @size: size of array
+ * @c: characters
+ * Return: a character
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int pos;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	buffer = (char *) malloc(size * sizeof(c));
+	if (buffer == o)
+	{
+		return (NULL);
+	}
+	else
+	{
+		pos = 0;
+		while (pos < size)
+		{
+			*(buffer + pos) = c;
+			pos++;
+		}
+	}
+	return (buffer);
+}
