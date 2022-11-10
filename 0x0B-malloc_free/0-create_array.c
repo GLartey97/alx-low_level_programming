fac722f021524d7d01f0ebbd6fd10afcfc1bc6d0
Gizer <gabriellartey16@gmail.com>
2022-11-10 14:13:51 +0000

    create array of chars updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * create_array - creates array of chars
@@ -17,7 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	buffer = (char *) malloc(size * sizeof(c));
-	if (buffer == o)
+	if (buffer == 0)
 	{
 		return (NULL);
 	}
