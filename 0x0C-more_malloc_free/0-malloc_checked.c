22fd5c4a8e21e2759bb9f9e93761c0c88c302650
Gizer <gabriellartey16@gmail.com>
2022-11-15 13:08:51 +0000

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: non negative int
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
