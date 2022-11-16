1e9c54aa1d8d506af41ffb75ca7056d678b99fc6
Gizer <gabriellartey16@gmail.com>
2022-11-16 01:28:23 +0000

    print struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,10 +6,11 @@
  * print_dog - prints a struct dog
  * @d: struct dog to print
  */
+
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		return (NULL);
+		return;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
