39079813174467c609761f63c98078f75cd5b54d
Gizer <gabriellartey16@gmail.com>
2022-11-17 16:45:50 +0000

    simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+
+/**
+ * main - simple calculator
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int arg1, arg2, results;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	o = *argv[2];
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	results = func(arg1, arg2);
+	printf("%d\n", results);
+	return (0);
+}
