4a6d8e4b1a420da09e83f54011c65b81cc6151ec
Gabriel Lartey <gabriellartey16@gmail.com>
2023-02-11 19:48:11 +0000

    3-hash_table_set.c updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,25 +13,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (key == NULL || strcmp(key, "") == 0)
+	if (!ht || !key || !strlen(key) || !value)
 	{
 		return (0);
 	}
+	index = key_index((unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		return (0);
+	}
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
+	if (!new_node->key)
 	{
 		free(new_node);
 		return (0);
 	}
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
+	if (!new_node->value)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-	index = key_index((unsigned char *)key, ht->size);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
