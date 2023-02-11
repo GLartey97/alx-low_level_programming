9d8b36d9bd366e81f59213493d7f2872d31905f4
Gabriel Lartey <gabriellartey16@gmail.com>
2023-02-11 01:24:34 +0000

    2-key_index.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives you the index key
+ * @key: character key
+ * @size: int size
+ * Return: index of a key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size) {
+  unsigned long hash_value;
+
+  hash_value = hash(key);
+  return hash_value % size;
+}
+
